cf6b509b8bffc005b941c1a183e674a407610431
She Chee Yee <shecheeyee@Shes-MacBook-Air.local>
2024-03-29 22:57:47 +0800

    testing flask app

--- src/app.py
+++ src/app.py
@@ -1,1 +1,34 @@
-# flask app
+# testing
+from typing import List, Dict
+from flask import Flask
+import mysql.connector
+import json
+
+app = Flask(__name__)
+
+
+def testing() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'mflg'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM employees')
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return result
+
+
+@app.route('/')
+def index() -> str:
+    return json.dumps({'employees': testing()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
