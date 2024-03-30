5137214a9454fcb7ff4dd45a1eb8077a84b127a4
She Chee Yee <shecheeyee@Shes-MacBook-Air.local>
2024-03-30 14:08:00 +0800

    fix: docker and db config

--- mysql_db/01-schema.sql
+++ mysql_db/01-schema.sql
@@ -1,10 +1,11 @@
+USE mflg;
+
 CREATE TABLE IF NOT EXISTS Employees (
   emp_id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   age INT NOT NULL,
   email VARCHAR(50) NOT NULL,
-  number VARCHAR(50) NOT NULL,
   gender ENUM('M', 'F') NOT NULL,
   primary_role ENUM('Manager', 'Server', 'Cook', 'Dishwasher'),
   secondary_role ENUM('Manager', 'Server', 'Cook', 'Dishwasher'),
@@ -13,26 +14,28 @@ CREATE TABLE IF NOT EXISTS Employees (
   address VARCHAR(256)
 );
 
+
+CREATE TABLE IF NOT EXISTS Availability (
+  emp_id INT REFERENCES Employees(emp_id),
+  week DATE NOT NULL,
+  mon TINYINT NOT NULL CHECK (mon >= 0 AND mon <= 3),  -- Values: 0 (Unavailable), 1 (Shift 1), 2 (Shift 2), 3 (Both)
+  tues TINYINT NOT NULL CHECK (tues >= 0 AND tues <= 3),
+  wed TINYINT NOT NULL CHECK (wed >= 0 AND wed <= 3),
+  thur TINYINT NOT NULL CHECK (thur >= 0 AND thur <= 3),
+  fri TINYINT NOT NULL CHECK (fri >= 0 AND fri <= 3),
+  sat TINYINT NOT NULL CHECK (sat >= 0 AND sat <= 3),
+  sun TINYINT NOT NULL CHECK (sun >= 0 AND sun <= 3),
+  CONSTRAINT unique_availability UNIQUE (emp_id, week)
+);
+
 CREATE TABLE IF NOT EXISTS Schedules (
   scheduleId INT PRIMARY KEY AUTO_INCREMENT,
-  emp_id INT FOREIGN KEY REFERENCES Employees(emp_id),
+  emp_id INT,
   week DATE NOT NULL,
   day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
   shift ENUM('Unavailable', 'Shift 1', 'Shift 2', 'Both') NOT NULL,
-  CONSTRAINT unique_schedule UNIQUE (employeeId, week, day)
-);
-
-CREATE TABLE IF NOT EXISTS Availability (
-  emp_id INT PRIMARY KEY REFERENCES Employees(emp_id),
-  week DATE NOT NULL,
-  mon TINYINT NOT NULL CHECK (mon >= 0 AND mon <= 3),  -- Values: 0 (Unavailable), 1 (Shift 1), 2 (Shift 2), 3 (Both)
-  tues TINYINT NOT NULL CHECK (tues >= 0 AND mon <= 3),
-  wed TINYINT NOT NULL CHECK (wed >= 0 AND mon <= 3),
-  thur TINYINT NOT NULL CHECK (thur >= 0 AND mon <= 3),
-  fri TINYINT NOT NULL CHECK (fri >= 0 AND mon <= 3),
-  sat TINYINT NOT NULL CHECK (sat >= 0 AND mon <= 3),
-  sun TINYINT NOT NULL CHECK (sun >= 0 AND mon <= 3),
-  CONSTRAINT unique_availability UNIQUE (employeeId, week)
+  CONSTRAINT unique_schedule UNIQUE (emp_id, week, day),
+  FOREIGN KEY (emp_id) REFERENCES Employees(emp_id)
 );
 
 
@@ -40,8 +43,8 @@ CREATE TABLE IF NOT EXISTS Events (
   event_id INT PRIMARY KEY AUTO_INCREMENT,
   date DATE NOT NULL,
   event_type VARCHAR(50),
-  eventName VARCHAR(50) NOT NULL,
-  numPax INT,
+  event_name VARCHAR(50) NOT NULL,
+  num_pax INT,
   time TIME NOT NULL,
   duration DECIMAL(5,2) NOT NULL
 );
@@ -62,8 +65,8 @@ CREATE TABLE IF NOT EXISTS SummaryStats (
   CONSTRAINT unique_summary_stats UNIQUE (date, shift)
 );
 
-CREATE TABLE IF NOT EXISTS Wage {
+CREATE TABLE IF NOT EXISTS Wage (
     day ENUM('Weekday', 'Weekend', 'Public Holiday'),
     role ENUM('Server', 'Cook', 'Dishwasher'),
     wage DECIMAL(10,2) NOT NULL
-};
+);
--- src/app.py
+++ src/app.py
@@ -1,33 +1,50 @@
 # testing
 from typing import List, Dict
 from flask import Flask
-import mysql.connector
+import pymysql
 import json
+import csv
 
 app = Flask(__name__)
 
 
-def testing() -> List[Dict]:
-    config = {
+config = {
         'user': 'root',
         'password': 'root',
-        'host': 'db',
+        'host': 'mysql-db',
         'port': '3306',
         'database': 'mflg'
     }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM employees')
-    result = cursor.fetchall()
-    cursor.close()
-    connection.close()
 
-    return result
+            
+def get_wage() -> List[Dict]:
+    """
+    Test: fetches wage data from the database and returns as a list of dictionaries.
+    """
+    config = config
+
+    try:
+        connection = pymysql.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM Wage')
+        result = cursor.fetchall()
+        return [{'day': row[0], 'role': row[1], 'wage':row[2]} for row in result]  
+
+    except Exception as e:
+        print(f"Error fetching wage: {e}")
+        return []  
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
 
 
 @app.route('/')
 def index() -> str:
-    return json.dumps({'employees': testing()})
+    wage = get_wage()
+    return json.dumps({'wage': employees})
 
 
 if __name__ == '__main__':
