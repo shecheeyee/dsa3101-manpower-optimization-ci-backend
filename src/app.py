ad099b1cc24e04dac4581a44e93f15a7a10a83ff
Ruirong <105039474+ruironggg@users.noreply.github.com>
2024-04-08 12:13:14 +0800

    fix: Modify store_demand_forecast endpoint implementation

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from db_utils import execute_query
 from model.employee import  create_employee, update_employee, get_all_employees, delete_employee
-from demand_forecast import demand_forecast
+from demand_forecast import seven_days_demand_forecast
 import json
 import pandas as pd
 
@@ -48,19 +48,13 @@ def get_wage():
 
 @app.route("/post_demand_forecast", methods=["POST"])
 def store_demand_forecast():
-    try:
-        # Get the forecast data
-        forecast_demand = demand_forecast()
-
-        # Insert the forecast data into the database
-        for date, day, time, hourly_customers in forecast_demand:
-            field_names = f"Date, Day, Time, expectedCustomers"
-            field_values = f"DATE'{date}', {day}, TIME '{time}', '{hourly_customers}'"
-            query = f"INSERT INTO DemandForecast ({field_names}) VALUES ({field_values})"
-            execute_query(query)
-        return jsonify({"message": "Data stored successfully"})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    forecast_demand = seven_days_demand_forecast() # forecast demand for today and next 6 days
+    for index, row in forecast_demand.iterrows():
+        field_names = f"Date, Day, Time, expectedCustomers"
+        field_values = f"DATE '{row['Date']}', '{row['Day']}', TIME '{row['Time']}', '{row['ExpectedCustomers']}'"
+        query = f"INSERT INTO DemandForecast ({field_names}) VALUES ({field_values})"
+        execute_query(query)
+    return jsonify({"message": "Data stored successfully"})
 
 @app.route("/get_demand_forecast", methods=["GET"])
 def get_demand_forecast():
@@ -88,4 +82,4 @@ def get_past_demand():
     return jsonify(past_demand_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
