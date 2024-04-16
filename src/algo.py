05b24f3411637fecc8e3543e008a6fb6531c8a6a
mersoncheong <merson.cheong@gmail.com>
2024-04-17 00:26:34 +0800

    feat: adjusted LP constraints to factor in fringe case and reorgnised code

--- src/algo.py
+++ src/algo.py
@@ -13,69 +13,157 @@ connection = pymysql.connect(
     host='127.0.0.1',
     user='root',
     password='root',  # Replace with actual password or environment variable
-    port=3306,  # Adjust if necessary
+    port=3307,  # Adjust if necessary
     database='mflg'
 )
 
- 
-
-def staffing_algorithm(ft_hours = 44, pt_hours = 35):
 # Define global variables
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    weekend = ['Saturday', 'Sunday']
-    global_week = get_global_week()
-    df_wage = wage_query()
-    df = get_employee_data()
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+weekend = ['Saturday', 'Sunday']
+global_week = get_global_week()
+df_wage = wage_query()
+df = get_employee_data()
+df_avail = get_avail_data(global_week)
+df_events = get_event_data()
 
-    df_avail = get_avail_data(global_week)
-    df_events = get_event_data()
+# get worker status
+def get_status(worker_id):
+    status = df[df['emp_id'] == worker_id]['status'].iloc[0]
+    return status == 'Full time'
 
+# get worker role
+def get_role(worker_id):
+    return df[df['emp_id'] == worker_id]['primary_role'].iloc[0]
+
+# get dates of the days in the current week
+def get_dates_and_day():
+    today = datetime.now()
+    sunday = today - timedelta(days=today.weekday() + 1)
+    dates_of_week = [sunday + timedelta(days=i) for i in range(7)]
+    dates_of_week = [date_obj.date() for date_obj in dates_of_week]
+    dates_of_week = [d.strftime("%Y-%m-%d") for d in dates_of_week]
+    return dates_of_week
+
+# Define your functions with assumed business rules
+def kitchen_required_func(x):
+    if x < 30:
+        return 3
+    elif x < 70:
+           return 4
+    else: 
+        return 5
+def service_required_func(x):
+    if x < 30:
+        return 4
+    elif x < 70:
+        return 5
+    else: 
+        return 6
+
+def service_shift_duration(shift_number):
+    if shift_number == 1 or shift_number == 2:
+        return 8
+    else:   
+        return 12
+def kitchen_shift_duration(shift_number):
+    if shift_number == 1 or shift_number == 2:
+        return 10   
+    else:
+        return 14
+
+def get_public_holidays():
+    dates_of_week = get_dates_and_day()
+    ph_week = [is_holiday(date) for date in dates_of_week]
+    ph_dict = dict(zip(days, ph_week))
+    return ph_dict
+
+def shift_duration(workerid,shiftindex):
+    status = get_status(workerid)
+    role = get_role(workerid)
+    if status: 
+        return 0
+    else: 
+        if role == 'Kitchen':
+            return kitchen_shift_duration(shiftindex)
+        else:
+            return service_shift_duration(shiftindex)
+            
+def service_pt_wage(day,shift):
+    ph_dict = get_public_holidays()
+    df_service_wage = df_wage[df_wage['role'] == 'Service']
+    duration = service_shift_duration(shift)
+    day_type = 'Weekday' if day in weekday else 'Weekend'
+    if ph_dict[day]:
+        return df_service_wage[df_service_wage['day'] == 'Public Holiday']['wage'].iloc[0] * duration
+    return df_service_wage[df_service_wage['day'] == day_type]['wage'].iloc[0] * duration
+
+def kitchen_pt_wage(day,shift): 
+    ph_dict = get_public_holidays()
+    df_kitchen_wage = df_wage[df_wage['role'] == 'Kitchen']
+    duration = kitchen_shift_duration(shift)
+    day_type = 'Weekday' if day in weekday else 'Weekend'
+    if ph_dict[day]:
+        return df_kitchen_wage[df_kitchen_wage['day'] == 'Public Holiday']['wage'].iloc[0] * duration
+    return df_kitchen_wage[df_kitchen_wage['day'] == day_type]['wage'].iloc[0] * duration
+
+
+def get_wage(worker_id,day,shift):
+    cost = 1
+    if not get_status(worker_id):
+        if get_role(worker_id) == 'Kitchen':
+            cost = kitchen_pt_wage(day,shift)
+        else:
+            cost = service_pt_wage(day,shift)
+    return cost
+
+def get_sec_role(worker_id):
+    return df[df['emp_id'] == worker_id]['secondary_role'].iloc[0]
+
+def get_avail(workerid, day,shifttype):
+    day_value = df_avail.loc[df_avail['emp_id'] == workerid][day].values[0]
+    if day_value == 'None':
+        return 0
+    elif day_value == 'Morning':
+        return 1 if shifttype == 1 else 0
+    elif day_value == 'Night':
+        return 1 if shifttype == 2 else 0
+    elif day_value == 'Full':
+        return 1
+
+def shift_duration(workerid, shift):
+    status = get_status(workerid)
+    role = get_role(workerid)
+    if status: 
+         return 0
+    else: 
+        if role == 'Kitchen':
+            return kitchen_shift_duration(shift)
+        else:
+            return service_shift_duration(shift)
+            
+
+    
+workers = df['emp_id'].tolist()  # 10 workers available, more than needed
+kitchen_workers = [worker for worker in workers if get_role(worker) == 'Kitchen']
+server_workers = [worker for worker in workers if get_role(worker) == 'Service']
+managers = [worker for worker in workers if get_role(worker) == 'Manager']
+
+
+def shift_algorithm():
     data = seven_days_demand_forecast().iloc[:, -3:]
     pivot_df = data.pivot_table(index='Time', columns='Day', values='ExpectedCustomers', fill_value=0)
     pivot_df = pivot_df[days]
     pivot_df.index.name = None
     pivot_df.columns.name = None
-
-    def get_status(worker_id):
-        status = df[df['emp_id'] == worker_id]['status'].iloc[0]
-        return status == 'Full time'
-
-    def get_role(worker_id):
-        return df[df['emp_id'] == worker_id]['primary_role'].iloc[0]
-
-
-    workers = df['emp_id'].tolist()  # 10 workers available, more than needed
-    kitchen_workers = [worker for worker in workers if get_role(worker) == 'Kitchen']
-    server_workers = [worker for worker in workers if get_role(worker) == 'Service']
-    managers = [worker for worker in workers if get_role(worker) == 'Manager']
-
-
     # Transpose the DataFrame to have times as rows and days as columns
     hrs = ['1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2100']
     pivot_df.index = hrs
-
-
-    def get_dates_and_day():
-        # Get today's date
-        today = datetime.now()
-        # Calculate the date of the previous Sunday
-        sunday = today - timedelta(days=today.weekday() + 1)
-        # Calculate the dates for the current week starting from Sunday
-        dates_of_week = [sunday + timedelta(days=i) for i in range(7)]
-        return dates_of_week
-
-    dates_of_week = get_dates_and_day()
-    dates_of_week = [date_obj.date() for date_obj in dates_of_week]
-    dates_of_week = [d.strftime("%Y-%m-%d") for d in dates_of_week]
-    ph_week = [is_holiday(date) for date in dates_of_week]
-    ph_dict = dict(zip(days, ph_week))
-    current_week = pd.DataFrame({"date" : dates_of_week, "day" : days})
+    current_week = pd.DataFrame({"date" : get_dates_and_day(), "day" : days})
     df_events['date'] = pd.to_datetime(df_events['date']).dt.strftime('%Y-%m-%d')
     joined_df = current_week.merge(df_events, on='date', how='inner') # only events that are in the current week
     selected_columns = ['day', 'event_period', 'staffReq']
     extracted_df = joined_df[selected_columns]
-
     # Array representing shifting hours
     timings = np.array([[1,0,1],
                     [1,1,1],
@@ -84,23 +172,6 @@ def staffing_algorithm(ft_hours = 44, pt_hours = 35):
     num_shifts = timings.shape[1]
     # number of time windows
     num_time = timings.shape[0]
-    # Define your functions with assumed business rules
-    def kitchen_required_func(x):
-        if x < 30:
-            return 3
-        elif x < 70:
-            return 4
-        else: 
-            return 5
-
-    def service_required_func(x):
-        if x < 30:
-            return 4
-        elif x < 70:
-            return 5
-        else: 
-            return 6
-
     # Map the functions over all entries in the DataFrame and replace them
     df_kitchen_mapped = pivot_df.map(kitchen_required_func)
     df_service_mapped = pivot_df.map(service_required_func)
@@ -180,90 +251,19 @@ def staffing_algorithm(ft_hours = 44, pt_hours = 35):
         for shift in shifts:
             temp_lst.append(workers_per_shift[shift].value())
         service_staff_dict[day] = temp_lst
-
     print(service_staff_dict)
+    return [service_staff_dict, kitchen_staff_dict]
 
-    service_shift_duration = {1: 8, 
-                    2: 8,
-                    3: 12}  # dict with shift as key and its duration in hours as value
-    # Cook run on different shifts
-    kitchen_shift_duration = {1: 10, 
-                        2: 10,
-                        3: 14}  # dict with shift as key and its duration in hours as value
-
-
-    def shift_duration(workerid,shiftindex):
-        status = get_status(workerid)
-        role = get_role(workerid)
-        if status: 
-            return 0
-        else: 
-            if role == 'Kitchen':
-                return kitchen_shift_duration[shiftindex]
-            else:
-                return service_shift_duration[shiftindex]
-            
-    def service_pt_wage(day,shift):
-        df_service_wage = df_wage[df_wage['role'] == 'Service']
-        duration = service_shift_duration[shift]
-        day_type = 'Weekday' if day in weekday else 'Weekend'
-        if ph_dict[day]:
-            return df_service_wage[df_service_wage['day'] == 'Public Holiday']['wage'].iloc[0] * duration
-        return df_service_wage[df_service_wage['day'] == day_type]['wage'].iloc[0] * duration
-
-    def kitchen_pt_wage(day,shift): 
-        df_kitchen_wage = df_wage[df_wage['role'] == 'Kitchen']
-        duration = kitchen_shift_duration[shift]
-        day_type = 'Weekday' if day in weekday else 'Weekend'
-        if ph_dict[day]:
-            return df_kitchen_wage[df_kitchen_wage['day'] == 'Public Holiday']['wage'].iloc[0] * duration
-        return df_kitchen_wage[df_kitchen_wage['day'] == day_type]['wage'].iloc[0] * duration
-
-
-    def get_wage(worker_id,day,shift):
-        cost = 0
-        if not get_status(worker_id):
-            if get_role(worker_id) == 'Kitchen':
-                cost = kitchen_pt_wage(day,shift)
-            else:
-                cost = service_pt_wage(day,shift)
-        return cost
-
-    def get_sec_role(worker_id):
-        return df[df['emp_id'] == worker_id]['secondary_role'].iloc[0]
-
-    def get_avail(workerid, day,shifttype):
-        try:
-            day_value = df_avail.loc[df_avail['emp_id'] == workerid][day].values[0]
-            if day_value == 'None':
-                return 0
-            elif day_value == 'Morning':
-                return 1 if shifttype == 1 else 0
-            elif day_value == 'Night':
-                return 1 if shifttype == 2 else 0
-            elif day_value == 'Full':
-                return 1
-        except:
-            print(workerid, day, shifttype)
-
-
-    def shift_duration(workerid, shift):
-        status = get_status(workerid)
-        role = get_role(workerid)
-        if status: 
-            return 0
-        else: 
-            if role == 'Kitchen':
-                return kitchen_shift_duration[shift]
-            else:
-                return service_shift_duration[shift]
-            
+def staffing_algorithm(ft_hours = 44, pt_hours = 35):
+    opt_shift = shift_algorithm()
+    kitchen_staff_dict,service_staff_dict = opt_shift[1], opt_shift[0]
 
     def kitchen_required(day, shift):
         return kitchen_staff_dict[day][shift-1]
 
     def service_required(day, shift):
         return service_staff_dict[day][shift-1]
+    ph_dict = get_public_holidays()
 
 
     problem = LpProblem("Minimize Staff Costs", LpMinimize)
@@ -321,52 +321,52 @@ def staffing_algorithm(ft_hours = 44, pt_hours = 35):
         
     # Define the constraint that each day and shift has optimised number of workers
     for day in days:
-        for shift_type in range(1,4):
+        for shift_type in range(1,3):
             if shift_type == 1:
                 shift_var = morning_shift
             elif shift_type == 2:
                 shift_var = night_shift
-            else:
-                shift_var = full_shift
-            problem += lpSum(shift_var[(day, worker)] for worker in kitchen_workers) == kitchen_required(day,shift_type)
-            problem += lpSum(shift_var[(day, worker)] for worker in server_workers) == service_required(day,shift_type)   
-
-
-    problem.solve(solver = PULP_CBC_CMD(msg=0))
+            problem += lpSum([shift_var[(day, worker)] + full_shift[(day,worker)] for worker in kitchen_workers]) >= kitchen_required(day,shift_type) + kitchen_required(day,3)
+            problem += lpSum([shift_var[(day, worker)] + full_shift[(day,worker)] for worker in server_workers]) >= service_required(day,shift_type) + service_required(day,3)
+        
+    problem.solve(solver = PULP_CBC_CMD(msg=0)) 
     status = LpStatus[problem.status]
-
     # Insert into Schema DB
+    kitchen_am_start = '08:00:00'
+    service_am_start = '10:00:00'
+    am_end = '18:00:00'
+    pm_start = '12:00:00'
+    pm_end = '22:00:00'
     for day in days:
         for worker in kitchen_workers:
             if morning_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Morning', 'Kitchen')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Morning', 'Kitchen','{kitchen_am_start}','{am_end}')"
                 execute_query(query)
             elif night_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Night', 'Kitchen')"
-                execute_query(query)    
-            elif full_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Full', 'Kitchen')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Night', 'Kitchen', '{pm_start}','{pm_end}')"
                 execute_query(query)
+            elif full_shift[(day, worker)].varValue == 1:
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Full', 'Kitchen', '{kitchen_am_start}','{pm_end}')"
+                execute_query(query)
+                print(query)
         for worker in server_workers:
             if morning_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Morning', 'Service')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime)) VALUES ({worker},'{global_week}','{day}','Morning', 'Service', '{service_am_start}','{am_end}')"
                 execute_query(query)
             elif night_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Night', 'Service')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Night', 'Service', '{pm_start}','{pm_end}')"
                 execute_query(query)
             elif full_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Full', 'Service')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Full', 'Service', '{service_am_start}','{pm_end}')"
                 execute_query(query)
         for worker in managers:
             if morning_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Morning', 'Manager')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Morning', 'Manager','{service_am_start}','{am_end}')"
                 execute_query(query)
             elif night_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Night', 'Manager')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Night', 'Manager', '{pm_start}','{pm_end}')"
                 execute_query(query)
             elif full_shift[(day, worker)].varValue == 1:
-                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role) VALUES ({worker},'{global_week}','{day}','Full', 'Manager')"
+                query = f"INSERT INTO Schedules (emp_id, week,day, shift,role,starttime,endtime) VALUES ({worker},'{global_week}','{day}','Full', 'Manager', '{service_am_start}','{pm_end}')"
                 execute_query(query)
     return status
-
-staffing_algorithm()
